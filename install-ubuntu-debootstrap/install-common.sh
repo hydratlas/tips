ff2b682cb48667708ba81f0b341c3e9ded0fe1da
hydratlas <160527126+hydratlas@users.noreply.github.com>
2024-02-29 01:31:28 +0900

    Create install-common.sh

---
+++ install-ubuntu-debootstrap/install-common.sh
@@ -0,0 +1,38 @@
+#!/bin/bash -eu
+function diskname-to-disk () {
+  if [ -n "${1}" ]; then
+    DISK1="/dev/${1}"
+  else
+    DISK1=""
+  fi
+  if [ -n "${2}" ]; then
+    DISK2="/dev/${2}"
+  else
+    DISK2=""
+  fi
+}
+
+function disk-to-partition () {
+  DISK1_EFI="${1}1"
+  DISK1_SWAP="${1}2"
+  DISK1_ROOTFS="${1}3"
+  if [ -e "${2}" ]; then
+    DISK2_EFI="${2}1"
+    DISK2_SWAP="${2}2"
+    DISK2_ROOTFS="${2}3"
+  fi
+}
+
+function mount-installfs () {
+  mount "${DISK1_ROOTFS}" -o "${BTRFS_OPTIONS},subvol=@" "${MOUNT_POINT}"
+  mkdir -p "${MOUNT_POINT}/root"
+  mount "${DISK1_ROOTFS}" -o "${BTRFS_OPTIONS},subvol=@root" "${MOUNT_POINT}/root"
+  mkdir -p "${MOUNT_POINT}/var/log"
+  mount "${DISK1_ROOTFS}" -o "${BTRFS_OPTIONS},subvol=@var_log" "${MOUNT_POINT}/var/log"
+  mkdir -p "${MOUNT_POINT}/boot/efi"
+  mount "${DISK1_EFI}" "${MOUNT_POINT}/boot/efi"
+  if [ -e "${DISK2}" ]; then
+    mkdir -p "${MOUNT_POINT}/boot/efi2"
+    mount "${DISK2_EFI}" "${MOUNT_POINT}/boot/efi2"
+  fi
+}
