2c1eb1053aa5f67742b88f5dd378819fc954047a
hydratlas <160527126+hydratlas@users.noreply.github.com>
2024-03-01 01:49:06 +0900

    Update install-mount.sh

--- install-ubuntu-debootstrap/install-mount.sh
+++ install-ubuntu-debootstrap/install-mount.sh
@@ -1,14 +1,6 @@
 #!/bin/bash -eu
-if [ -n "${1}" ]; then
-	DISK1="${1}"
-else
-	DISK1=""
-fi
-if [ -n "${2}" ]; then
-	DISK2="${2}"
-else
-	DISK2=""
-fi
+DISK1="${1:-}"
+DISK2="${2:-}"
 
 source ./install-config.sh
 source ./install-common.sh
