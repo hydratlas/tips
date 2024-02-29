2bc0e72b33a0f3ed90a98933678f41d584d74f6e
hydratlas <160527126+hydratlas@users.noreply.github.com>
2024-02-29 20:19:53 +0900

    Update install-common.sh

--- install-ubuntu-debootstrap/install-common.sh
+++ install-ubuntu-debootstrap/install-common.sh
@@ -1,36 +1,38 @@
 #!/bin/bash -eu
-function diskname-to-disk () {
-  if [ -n "${1}" ]; then
-    DISK1="/dev/${1}"
-  else
-    DISK1=""
-  fi
-  if [ -n "${2}" ]; then
-    DISK2="/dev/${2}"
-  else
-    DISK2=""
-  fi
+function diskname-to-diskpath () {
+	if [ -n "${1}" ]; then
+		DISK1="/dev/${1}"
+	else
+		DISK1=""
+	fi
+	if [ -n "${2}" ]; then
+		DISK2="/dev/${2}"
+	else
+		DISK2=""
+	fi
 }
 
 function disk-to-partition () {
-  DISK1_EFI="${1}1"
-  DISK1_SWAP="${1}2"
-  DISK1_ROOTFS="${1}3"
-  if [ -e "${2}" ]; then
-    DISK2_EFI="${2}1"
-    DISK2_SWAP="${2}2"
-    DISK2_ROOTFS="${2}3"
-  fi
+	if [ -e "${1}" ]; then
+		DISK1_EFI="${1}1"
+		DISK1_SWAP="${1}2"
+		DISK1_ROOTFS="${1}3"
+	fi
+	if [ -e "${2}" ]; then
+		DISK2_EFI="${2}1"
+		DISK2_SWAP="${2}2"
+		DISK2_ROOTFS="${2}3"
+	fi
 }
 
 function mount-installfs () {
-  mount "${DISK1_ROOTFS}" -o "${BTRFS_OPTIONS},subvol=@" --mkdir "${MOUNT_POINT}"
-  mount "${DISK1_ROOTFS}" -o "${BTRFS_OPTIONS},subvol=@root" --mkdir "${MOUNT_POINT}/root"
-  mount "${DISK1_ROOTFS}" -o "${BTRFS_OPTIONS},subvol=@var_log" --mkdir "${MOUNT_POINT}/var/log"
-  mount "${DISK1_ROOTFS}" -o "${BTRFS_OPTIONS},subvol=@snapshots" --mkdir "${MOUNT_POINT}/.snapshots"
+	mount "${DISK1_ROOTFS}" -o "${BTRFS_OPTIONS},subvol=@" --mkdir "${MOUNT_POINT}"
+	mount "${DISK1_ROOTFS}" -o "${BTRFS_OPTIONS},subvol=@root" --mkdir "${MOUNT_POINT}/root"
+	mount "${DISK1_ROOTFS}" -o "${BTRFS_OPTIONS},subvol=@var_log" --mkdir "${MOUNT_POINT}/var/log"
+	mount "${DISK1_ROOTFS}" -o "${BTRFS_OPTIONS},subvol=@snapshots" --mkdir "${MOUNT_POINT}/.snapshots"
 
-  mount "${DISK1_EFI}" --mkdir "${MOUNT_POINT}/boot/efi"
-  if [ -e "${DISK2}" ]; then
-    mount "${DISK2_EFI}" --mkdir "${MOUNT_POINT}/boot/efi2"
-  fi
+	mount "${DISK1_EFI}" --mkdir "${MOUNT_POINT}/boot/efi"
+	if [ -e "${DISK2}" ]; then
+		mount "${DISK2_EFI}" --mkdir "${MOUNT_POINT}/boot/efi2"
+	fi
 }
