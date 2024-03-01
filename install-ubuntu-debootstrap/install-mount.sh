f57f3ad614656092153d4eecb20ad42743a8e55e
hydratlas <160527126+hydratlas@users.noreply.github.com>
2024-03-01 22:40:03 +0900

    Update install-mount.sh

--- install-ubuntu-debootstrap/install-mount.sh
+++ install-ubuntu-debootstrap/install-mount.sh
@@ -5,5 +5,5 @@ DISK2="${2:-}"
 source ./install-config.sh
 source ./install-common.sh
 diskname-to-diskpath "${DISK1}" "${DISK2}"
-disk-to-partition "${DISK1_PATH}" "${DISK2_PATH}"
+diskpath-to-partitionpath "${DISK1_PATH}" "${DISK2_PATH}"
 mount-installfs
