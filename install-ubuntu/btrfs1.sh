28e8562b2458386000d7c3ac172dd183cb4071c9
hydratlas <160527126+hydratlas@users.noreply.github.com>
2024-03-02 05:04:53 +0900

    Update btrfs1.sh

--- install-ubuntu/btrfs1.sh
+++ install-ubuntu/btrfs1.sh
@@ -9,7 +9,7 @@ sudo sgdisk \
   -Z \
   -n 0::256MiB -t 0:ef00 \
   -n 0::4GiB   -t 0:8200 \
-  -n 0::       -t 0:fd00 "$DISK"
+  -n 0::       -t 0:8304 "$DISK"
 
 sudo mkfs.vfat -F 32 "${DISK}1"
 sudo mkswap "${DISK}2"
