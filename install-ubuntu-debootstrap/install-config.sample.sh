94af2fda4d9286a224cd791553040a447404496f
hydratlas <160527126+hydratlas@users.noreply.github.com>
2024-03-02 01:48:00 +0900

    Update install-config.sample.sh

--- install-ubuntu-debootstrap/install-config.sample.sh
+++ install-ubuntu-debootstrap/install-config.sample.sh
@@ -21,7 +21,7 @@ readonly PACKAGES_TO_INSTALL=( \
   libpam-systemd systemd-resolved systemd-timesyncd \
   e2fsprogs-l10n needrestart unattended-upgrades \
   dmidecode efibootmgr fwupd gdisk htop lshw lsof pci.ids pciutils usb.ids usbutils \
-  bzip2 curl git make moreutils nano perl psmisc rsync time uuid-runtime wget \
+  bzip2 curl git make moreutils nano perl psmisc rsync time uuid-runtime wget zstd \
   bash-completion command-not-found landscape-common \
   )
 readonly DEPENDENT_PACKAGES_TO_INSTALL=(ubuntu-minimal)
