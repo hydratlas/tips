4ef050ed24a503199899870c1c90a4830bf619e9
hydratlas <160527126+hydratlas@users.noreply.github.com>
2024-03-01 23:20:57 +0900

    Update install2.sh

--- install-ubuntu-debootstrap/install2.sh
+++ install-ubuntu-debootstrap/install2.sh
@@ -137,6 +137,11 @@ arch-chroot "${MOUNT_POINT}" useradd --password "${USER_PASSWORD}" --user-group 
 
 # Install SSH server
 DEBIAN_FRONTEND=noninteractive arch-chroot "${MOUNT_POINT}" apt-get install -y --no-install-recommends openssh-server
+tee "${MOUNT_POINT}/etc/ssh/ssh_config.d/20-local.conf" <<- EOS > /dev/null
+PasswordAuthentication no
+PermitRootLogin no
+EOS
+cat "${MOUNT_POINT}/etc/ssh/ssh_config.d/20-local.conf" # confirmation
 
 mkdir -p "${MOUNT_POINT}${USER_HOME_DIR}/.ssh"
 wget -O "${MOUNT_POINT}${USER_HOME_DIR}/.ssh/authorized_keys" "${PUBKEYURL}"
