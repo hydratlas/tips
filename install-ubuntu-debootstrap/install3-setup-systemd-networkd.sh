240dea7a0a0cd9dbca302614666499781c9c9d64
hydratlas <160527126+hydratlas@users.noreply.github.com>
2024-03-04 04:06:32 +0900

    Update install3-setup-systemd-networkd.sh

--- install-ubuntu-debootstrap/install3-setup-systemd-networkd.sh
+++ install-ubuntu-debootstrap/install3-setup-systemd-networkd.sh
@@ -10,6 +10,7 @@ function setup-systemd-networkd () {
 
 	arch-chroot "${MOUNT_POINT}" systemctl enable systemd-networkd
 
+	# Configure basic settings
 	tee "${MOUNT_POINT}/etc/systemd/network/20-wired.network" <<- EOS > /dev/null
 	[Match]
 	Name=en*
@@ -20,6 +21,7 @@ function setup-systemd-networkd () {
 	EOS
 	cat "${MOUNT_POINT}/etc/systemd/network/20-wired.network" # confirmation
 
+	# Configure Wake On LAN
 	tee "${MOUNT_POINT}/etc/systemd/network/50-wired.link" <<- EOS > /dev/null
 	[Match]
 	Name=en*
@@ -30,6 +32,14 @@ function setup-systemd-networkd () {
 	cat "${MOUNT_POINT}/etc/systemd/network/50-wired.link" # confirmation
 
 	perl -p -i -e "s/^#?MulticastDNS=.*\$/MulticastDNS=${MDNS_STR}/g" "${MOUNT_POINT}/etc/systemd/resolved.conf"
+
+	# If one interface can be connected, it will start without waiting.
+	mkdir -p "${MOUNT_POINT}/etc/systemd/system/systemd-networkd-wait-online.service.d"
+	tee "${MOUNT_POINT}/etc/systemd/system/systemd-networkd-wait-online.service.d/wait-for-only-one-interface.conf" <<- EOS > /dev/null
+	[Service]
+	ExecStart=
+	ExecStart=/usr/lib/systemd/systemd-networkd-wait-online --any
+	EOS
 }
 
 setup-systemd-networkd
