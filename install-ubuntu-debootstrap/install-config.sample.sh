ca7edcc422dbd61924028e1d024622df2a8bffb9
hydratlas <160527126+hydratlas@users.noreply.github.com>
2024-03-04 03:32:20 +0900

    Update install-config.sample.sh

--- install-ubuntu-debootstrap/install-config.sample.sh
+++ install-ubuntu-debootstrap/install-config.sample.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 readonly SUITE="mantic"
+
+# Storage
 readonly ROOT_FILESYSTEM="btrfs"
 readonly BTRFS_OPTIONS="defaults,ssd,noatime,space_cache=v2,discard=async,compress=zstd:1,degraded"
 readonly EXT4_OPTIONS="defaults,noatime"
@@ -7,23 +9,27 @@ readonly XFS_OPTIONS="defaults,noatime"
 readonly MOUNT_POINT="/mnt"
 readonly EFI_END="256MiB"
 readonly SWAP_END="4GiB"
+
+# Sebootstrap
 readonly INSTALLER="mmdebstrap"
 #readonly INSTALLER="debootstrap"
 readonly CACHE_DIR="/tmp/debootstrap"
 readonly MIRROR1="http://ftp.udx.icscoe.jp/Linux/ubuntu/"
 readonly MIRROR2="http://jp.archive.ubuntu.com/ubuntu/"
 readonly MIRROR3="http://archive.ubuntu.com/ubuntu/"
+
+# Basic Settings
 readonly INSTALLATION_LANG="C.UTF-8"
 readonly TIMEZONE="Asia/Tokyo"
 readonly XKBMODEL="pc105"
 readonly XKBLAYOUT="jp"
 readonly XKBVARIANT="OADG109A"
+
+# GRUB
 readonly GRUB_CMDLINE_LINUX_DEFAULT=""
 #readonly GRUB_CMDLINE_LINUX_DEFAULT="quiet splash"
-readonly SETUP_SYSTEMD_NETWORKD=true
-readonly WOL="magic"
-#readonly WOL="off"
-readonly MDNS=true
+
+# Packages
 readonly PACKAGES_TO_INSTALL=( \
   initramfs-tools linux-firmware linux-generic shim-signed \
   libpam-systemd systemd-resolved systemd-timesyncd \
@@ -35,7 +41,14 @@ readonly PACKAGES_TO_INSTALL=( \
   )
 readonly DEPENDENT_PACKAGES_TO_INSTALL=(ubuntu-minimal)
 readonly PACKAGES_NOT_INSTALL=(eject netplan.io ubuntu-advantage-tools vim-tiny)
+
+# User
 readonly USER_NAME="ubuntu"
 readonly USER_PASSWORD='$6$LMqzniAEoBSSS4gu$mMmV91M3oXrIpYCxIM2AlgvjxUWH2OPmLptPkttULMYMRCaJsfYxSiIySVM1q/K/mJVrAXnNNQEK9PTciP2Oe.'
 readonly USER_HOME_DIR="/home2/ubuntu"
 readonly USER_LANG="ja_JP.UTF-8"
+
+# systemd-networkd
+readonly WOL="magic"
+#readonly WOL="off"
+readonly MDNS=true
