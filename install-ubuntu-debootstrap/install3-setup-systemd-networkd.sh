ad8a1516bc2fef32476e66817cb1428914a4fe3d
hydratlas <160527126+hydratlas@users.noreply.github.com>
2024-03-04 03:36:24 +0900

    Update install3-setup-systemd-networkd.sh

--- install-ubuntu-debootstrap/install3-setup-systemd-networkd.sh
+++ install-ubuntu-debootstrap/install3-setup-systemd-networkd.sh
@@ -2,34 +2,34 @@
 source ./install-config.sh
 
 function setup-systemd-networkd () {
-  if ${MDNS}; then
-    local -r MDNS_STR="yes"
-  else
-    local -r MDNS_STR="no"
-  fi
+	if ${MDNS}; then
+		local -r MDNS_STR="yes"
+	else
+		local -r MDNS_STR="no"
+	fi
 
-  arch-chroot "${MOUNT_POINT}" systemctl enable systemd-networkd
+	arch-chroot "${MOUNT_POINT}" systemctl enable systemd-networkd
 
-  tee "${MOUNT_POINT}/etc/systemd/network/20-wired.network" <<- EOS > /dev/null
-  [Match]
-  Name=en*
+	tee "${MOUNT_POINT}/etc/systemd/network/20-wired.network" <<- EOS > /dev/null
+	[Match]
+	Name=en*
 
-  [Network]
-  DHCP=yes
-  MulticastDNS=${MDNS_STR}
-  EOS
-  cat "${MOUNT_POINT}/etc/systemd/network/20-wired.network" # confirmation
+	[Network]
+	DHCP=yes
+	MulticastDNS=${MDNS_STR}
+	EOS
+	cat "${MOUNT_POINT}/etc/systemd/network/20-wired.network" # confirmation
 
-  tee "${MOUNT_POINT}/etc/systemd/network/50-wired.link" <<- EOS > /dev/null
-  [Match]
-  Name=en*
+	tee "${MOUNT_POINT}/etc/systemd/network/50-wired.link" <<- EOS > /dev/null
+	[Match]
+	Name=en*
 
-  [Link]
-  WakeOnLan=${WOL}
-  EOS
-  cat "${MOUNT_POINT}/etc/systemd/network/50-wired.link" # confirmation
+	[Link]
+	WakeOnLan=${WOL}
+	EOS
+	cat "${MOUNT_POINT}/etc/systemd/network/50-wired.link" # confirmation
 
-  perl -p -i -e "s/^#?MulticastDNS=.*\$/MulticastDNS=${MDNS_STR}/g" "${MOUNT_POINT}/etc/systemd/resolved.conf"
+	perl -p -i -e "s/^#?MulticastDNS=.*\$/MulticastDNS=${MDNS_STR}/g" "${MOUNT_POINT}/etc/systemd/resolved.conf"
 }
 
 setup-systemd-networkd
