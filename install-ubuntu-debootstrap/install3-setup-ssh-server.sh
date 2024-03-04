e92409dddb6c1954b3a409c588d3aa9de881a7d1
hydratlas <160527126+hydratlas@users.noreply.github.com>
2024-03-05 02:52:55 +0900

    Update install3-setup-ssh-server.sh

--- install-ubuntu-debootstrap/install3-setup-ssh-server.sh
+++ install-ubuntu-debootstrap/install3-setup-ssh-server.sh
@@ -2,6 +2,7 @@
 source ./install-config.sh
 
 function setup-ssh-server () {
+	arch-chroot "${MOUNT_POINT}" apt-get update
 	DEBIAN_FRONTEND=noninteractive arch-chroot "${MOUNT_POINT}" apt-get install -y --no-install-recommends openssh-server
 	tee "${MOUNT_POINT}/etc/ssh/ssh_config.d/20-local.conf" <<- EOS > /dev/null
 	PasswordAuthentication no
