887c163fe8d7ec3df2f444fe21b1d9bebd2580ec
hydratlas <160527126+hydratlas@users.noreply.github.com>
2024-03-05 06:22:37 +0900

    Update install-config.sample.sh

--- install-ubuntu-debootstrap/install-config.sample.sh
+++ install-ubuntu-debootstrap/install-config.sample.sh
@@ -37,7 +37,7 @@ readonly VARIANT="standard"
 readonly PACKAGES_TO_INSTALL=( \
   linux-image-amd64 firmware-linux intel-microcode amd64-microcode \
   initramfs-tools libpam-systemd systemd-timesyncd \
-  e2fsprogs-l10n logrotate needrestart unattended-upgrades \
+  e2fsprogs-l10n logrotate needrestart sudo unattended-upgrades \
   dmidecode efibootmgr fwupd pci.ids pciutils usb.ids usbutils \
   bash-completion command-not-found \
   task-japanese \
