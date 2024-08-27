f65ce49e64f3e73f308f4d3aea7c9b531c747bcf
hydratlas <160527126+hydratlas@users.noreply.github.com>
2024-08-27 23:39:25 +0900

    Update btrfs1.sh NVMe対応

--- debian-and-ubuntu-tips/install-ubuntu-with-btrfs/btrfs1.sh
+++ debian-and-ubuntu-tips/install-ubuntu-with-btrfs/btrfs1.sh
@@ -11,5 +11,14 @@ sgdisk \
   -n 0::4GiB   -t 0:8200 \
   -n 0::       -t 0:8304 "$DISK"
 
-mkfs.vfat -F 32 "${DISK}1"
-mkswap "${DISK}2"
+if [ -e "${DISK}1" ]; then
+  mkfs.vfat -F 32 "${DISK}1"
+else
+  mkfs.vfat -F 32 "${DISK}p1"
+fi
+
+if [ -e "${DISK}2" ]; then
+  mkswap "${DISK}2"
+else
+  mkswap "${DISK}p2"
+fi
