1e6aa9ae2b20971f8ebd7704e8dc5003032e3e27
hydratlas <160527126+hydratlas@users.noreply.github.com>
2024-11-13 23:27:49 +0900

    修正

--- debian-and-ubuntu-tips/install-ubuntu-with-btrfs/update.sh
+++ debian-and-ubuntu-tips/install-ubuntu-with-btrfs/update.sh
@@ -42,9 +42,6 @@ btrfs subvolume delete "${TARGET}/${TMP_SNAPSHOT_NAME}" # 転送元の書き込�
 btrfs send "${TARGET}/${NEW_SNAPSHOT_NAME}" | btrfs receive . # 読み取り専用のスナップショットを転送
 btrfs subvolume delete "${TARGET}/${NEW_SNAPSHOT_NAME}" # 転送元の読み取り専用のスナップショットを削除
 
-# fstabをコピー
-cp -p "${DEFAULT_SUBVOLUME_NAME}/etc/fstab" "${TMP_SNAPSHOT_NAME}/etc/fstab"
-
 # @snapshotsサブボリュームがなければ
 if [ ! -e "${SNAPSHOTS_SUBVOLUME_NAME}" ]; then
   # サブボリューム作成
@@ -58,11 +55,14 @@ btrfs subvolume set-default .
 btrfs subvolume delete "${DEFAULT_SUBVOLUME_NAME}"
 
 # 新しい@サブボリュームのリネーム
-btrfs subvolume snapshot "${TMP_SNAPSHOT_NAME}" "${DEFAULT_SUBVOLUME_NAME}" # 新しいスナップショットを作ることによってリネーム
-btrfs subvolume delete "${TMP_SNAPSHOT_NAME}" # リネーム前のスナップショットを削除
+btrfs subvolume snapshot "${NEW_SNAPSHOT_NAME}" "${DEFAULT_SUBVOLUME_NAME}" # 新しいスナップショットを作ることによってリネーム
+btrfs subvolume delete "${NEW_SNAPSHOT_NAME}" # リネーム前のスナップショットを削除
 
 # @サブボリュームをデフォルト（GRUBがブートしようとする）に変更
 btrfs subvolume set-default "${DEFAULT_SUBVOLUME_NAME}"
+
+# fstabをコピー
+cp -p "${SNAPSHOTS_SUBVOLUME_NAME}/${OLD_SNAPSHOT_NAME}/etc/fstab" "${DEFAULT_SUBVOLUME_NAME}/etc/fstab"
 
 # 退避した既存の@サブボリュームから起動できるようにする
 ## fstabの修正
